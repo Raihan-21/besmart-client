7e44d050b03759cb6ea5b756be14568c724d521e
Muhammad Raihan <muhammadraihan118@gmail.com>
2022-07-06 22:38:09 +0700

    admin edit guru

--- src/pages/admin/EditGuru.jsx
+++ src/pages/admin/EditGuru.jsx
@@ -1,44 +1,94 @@
 import { Button, TextField } from "@mui/material";
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { useCallback } from "react";
 import useFetchDetail from "../../hooks/useFetchDetail";
 import { useParams } from "react-router-dom";
 import styles from "../../styles/Admin.module.css";
 const EditGuru = () => {
   const params = useParams();
   const [data, isLoading] = useFetchDetail(`/admin/guru/${params.id}`);
-  //   useEffect(() => {}, []);
+  const [formData, setFormData] = useState({
+    username: "",
+    nama: "",
+    alamat: "",
+    noHp: "",
+  });
+  useEffect(() => {
+    setFormData((prevState) => {
+      return {
+        ...prevState,
+        username: data.username,
+        nama: data.nama,
+        alamat: data.alamat,
+        noHp: data.no_hp,
+      };
+    });
+  }, [data]);
+  const formSubmit = useCallback(
+    async (e) => {
+      e.preventDefault();
+      const res = await fetch(`/admin/guru/${params.id}`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const resData = await res.json();
+      console.log(resData.data);
+    },
+    [formData]
+  );
   return (
     <div className={`${styles.adminContainer} ${styles.editAdmin}`}>
       <h3>Edit Guru</h3>
       {!isLoading && (
-        <form>
+        <form onSubmit={formSubmit}>
           <div className={styles.inputs}>
             <TextField
               label="Username"
               variant="outlined"
               margin="normal"
-              value={data.username}
+              value={formData.username || ""}
+              onChange={(e) => {
+                setFormData((prevState) => {
+                  return { ...prevState, username: e.target.value };
+                });
+              }}
             />
             <TextField
               label="Nama"
               variant="outlined"
               margin="normal"
-              value={data.nama}
+              value={formData.nama || ""}
+              onChange={(e) => {
+                setFormData((prevState) => {
+                  return { ...prevState, nama: e.target.value };
+                });
+              }}
             />
             <TextField
               label="Alamat"
               variant="outlined"
               margin="normal"
-              value={data.alamat}
+              value={formData.alamat || ""}
+              onChange={(e) => {
+                setFormData((prevState) => {
+                  return { ...prevState, alamat: e.target.value };
+                });
+              }}
             />
             <TextField
               label="No Hp"
               variant="outlined"
               margin="normal"
-              value={data.no_hp}
+              value={formData.noHp || ""}
+              onChange={(e) => {
+                setFormData((prevState) => {
+                  return { ...prevState, noHp: e.target.value };
+                });
+              }}
             />
           </div>
-          <Button color="primary" variant="contained">
+          <Button type="submit" color="primary" variant="contained">
             Save
           </Button>
         </form>
