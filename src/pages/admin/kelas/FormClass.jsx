e1f16915c4e7e947ded4517a176cf2aa476fdda4
Muhammad Raihan <muhammadraihan118@gmail.com>
2022-08-03 17:59:29 +0700

    add jadwal on kelas admin page

--- src/pages/admin/kelas/FormClass.jsx
+++ src/pages/admin/kelas/FormClass.jsx
@@ -1,21 +1,63 @@
 import styles from "../../../assets/styles/Admin.module.scss";
 import useFetch from "../../../hooks/useFetch";
 import { Button, TextField, Autocomplete } from "@mui/material";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { Grid } from "@mui/material";
-
+import {
+  Scheduler,
+  MonthView,
+  Appointments,
+  AppointmentTooltip,
+  AppointmentForm,
+  EditRecurrenceMenu,
+  Toolbar,
+  TodayButton,
+  DateNavigator,
+} from "@devexpress/dx-react-scheduler-material-ui";
+import { ViewState, EditingState } from "@devexpress/dx-react-scheduler";
+const appointments = [
+  {
+    title: "hari 1",
+    startDate: new Date(2022, 7, 1, 9, 45),
+    endDate: new Date(2022, 7, 1, 11, 0),
+    id: 0,
+  },
+];
 const FormClass = ({ formData, onSubmit }) => {
   const [data, setData] = useState({});
+  const [currentDate, setCurrentDate] = useState(new Date());
+  const [appointData, setAppointData] = useState(appointments);
   //eslint-disable-next-line
   const [guru, guruLoading] = useFetch("/admin/guru");
   //eslint-disable-next-line
   const [kategori, kategoriLoading] = useFetch("/admin/kategori");
+  const commitChanges = useCallback(
+    ({ added, changed }) => {
+      let currentData = [...appointData];
+      if (added) {
+        const startingAddedId =
+          appointData.length > 0
+            ? appointData[appointData.length - 1].id + 1
+            : 0;
+        currentData = [...currentData, { id: startingAddedId, ...added }];
+      }
+      if (changed) {
+        currentData = currentData.map((appointments) =>
+          changed[appointments.id]
+            ? { ...appointments, ...changed[appointments.id] }
+            : appointments
+        );
+      }
+      setAppointData(currentData);
+    },
+    [appointData]
+  );
   useEffect(() => {
     setData(formData);
   }, [formData]);
   return (
     <>
-      <div className={` ${styles.formContainer}`}>
+      <div className={` ${styles.formContainer} space-y-8`}>
         <form
           onSubmit={(e) => {
             e.preventDefault();
@@ -33,7 +75,6 @@ const FormClass = ({ formData, onSubmit }) => {
                   return { _id: opt._id, nama_kategori: opt.nama_kategori };
                 })}
                 value={data ? (data.kategori ? data.kategori : "") : ""}
-                // value={data ? (data.kategori ? data.kategori._id : "") : ""}
                 onChange={(e, value) => {
                   setData((prevState) => {
                     return {
@@ -85,6 +126,28 @@ const FormClass = ({ formData, onSubmit }) => {
             Save
           </Button>
         </form>
+        <div>
+          <h3>Jadwal</h3>
+
+          <Scheduler data={appointData}>
+            <ViewState
+              currentDate={currentDate}
+              onCurrentDateChange={(currentDate) => {
+                setCurrentDate(currentDate);
+              }}
+            />
+            <EditingState onCommitChanges={commitChanges} />
+            <Toolbar />
+
+            <DateNavigator />
+            <TodayButton />
+            <MonthView />
+            <EditRecurrenceMenu />
+            <Appointments />
+            <AppointmentTooltip showOpenButton showDeleteButton />
+            <AppointmentForm />
+          </Scheduler>
+        </div>
       </div>
     </>
   );
